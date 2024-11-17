ea371bb7a8f0a4fa7cb7b53818cad5ea69aa3565
IDHxxxB <161861603+IDHxxxB@users.noreply.github.com>
2024-11-17 23:00:18 +0800

    Part 5: Collection of Appoinments
    This section creates a collection of Appoinment using ArrayList, based on job requirements. In addition, three methods are created to create a new Appointment to add to ArrayList, to delete an appointment request from ArrayList, and to print all existing appointment information.

--- AssignmentOne.java
+++ AssignmentOne.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //Part 1
 class HealthProfessional {
     int id;
@@ -66,12 +68,12 @@ class Appointment{
         System.out.println("Name: " + name);
         System.out.println("Phone Number: " + phoneNum);
         System.out.println("Time: " + timeSlot);
-        System.out.println("Selected Doctor: " + selectedDoctor.name);
+        System.out.println("Selected Doctor: " + selectedDoctor.name + "\n");
 
     }
 
-    public String getName(){
-        return name;
+    public String getPhoneNum(){
+        return phoneNum;
     }
 }
 
@@ -93,13 +95,41 @@ public class AssignmentOne {
         N2.printInf();
         System.out.println("--------------------");
 
-        //Part 4 test
-        Appointment A1 = new Appointment("Zack", "123456", "8:00", N1);
-        Appointment A2 = new Appointment("Emily", "123457", "10:00", G2);
+        //Part 5 - Collection of appointment
+        ArrayList<Appointment> List = new ArrayList<>();
+        createAppointment(List, "Patient1", "10001", "8:00", G1);
+        createAppointment(List, "Patient2", "10002", "10:00", N1);
+        printExistingAppointments(List);
+        cancelBooking(List, "10002");
+        printExistingAppointments(List);
+        System.out.println("--------------------");
+    }
 
-        A1.printInf();
-        A2.printInf();
+    public static void createAppointment(ArrayList<Appointment> list, String name, String phoneNum, String timeSlot, HealthProfessional selectedDoctor) {
+        Appointment newAppointment = new Appointment(name, phoneNum, timeSlot, selectedDoctor);
+        list.add(newAppointment);
+    }
 
-        System.out.println(A1.getName());
+    public static void printExistingAppointments(ArrayList<Appointment> list) {
+        if (list.isEmpty()) {
+            System.out.println("There is no appointments in the database");
+        }
+        for (Appointment app : list) {
+            app.printInf();
+        }
+    }
+
+    public static void cancelBooking(ArrayList<Appointment> list, String phoneNum) {
+        boolean found = false;
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).getPhoneNum().equals(phoneNum)) {
+                list.remove(i);
+                found = true;
+                break;
+            }
+        }
+        if(!found) {
+            System.out.println(phoneNum + " could not be found. Can not cancel booking." + "\n");
+        }
     }
 }
